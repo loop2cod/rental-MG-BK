bea8888855c74bfdbb44a109f4b39fc6d20833e1
novus318 <nizamudheen318@gmail.com>
2025-05-11 09:46:22 +0530

    cors update

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [process.env.ALLOWED_ORIGIN1, process.env.ALLOWED_ORIGIN2],
+    origin: [process.env.ALLOWED_ORIGIN1, process.env.ALLOWED_ORIGIN2, process.env.ALLOWED_ORIGIN3],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
